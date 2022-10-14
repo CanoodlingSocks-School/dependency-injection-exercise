15ab422433b5682fe7216aef2e5c2a5c4e228686
Daniela Escobar <daniela.b.escobar@gmail.com>
2022-10-14 09:23:22 +0200

    Change EmployeeDAL to use an interface

--- dependency-injection-exercise/EmployeeDAL.cs
+++ dependency-injection-exercise/EmployeeDAL.cs
@@ -7,23 +7,6 @@ using System.Threading.Tasks;
 
 namespace dependency_injection_exercise
 {
-    //This class below demonstrate how EmployeeBL is tightly coupled with EmployeeDAL
-
-    public class EmployeeDAL
-    {
-        public List<Employee> SelectAllEmployees()
-        {
-            List<Employee> ListEmployees = new List<Employee>();
-            //Get Employees from Database (Hardcoded)
-
-            ListEmployees.Add(new Employee() { Id = 1, Name = "Harold", Department = "IT" });
-            ListEmployees.Add(new Employee() { Id = 2, Name = "Peter", Department = "HR" });
-            ListEmployees.Add(new Employee() { Id = 3, Name = "Annika", Department = "Payroll" });
-            return ListEmployees;
-        }
-    }
-    
-
      //Using the Constructor Dependency Injection Design Pattern to make EmployeeBL and EmployeeDAL loosely coupled
 
 
