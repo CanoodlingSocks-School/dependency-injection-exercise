f2683102ac301cc09922b0a4add569dc7ba8c635
Daniela Escobar <daniela.b.escobar@gmail.com>
2022-10-14 09:14:32 +0200

    Add project files.

---
+++ dependency-injection-exercise/EmployeeBL.cs
@@ -0,0 +1,26 @@
+﻿using dependency_injection_exercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dependency_injection_exercise
+{
+    // To get data we need to utilize the EmployeeDAL class.
+    // EmployeeBL depends on EmployeeDAL
+    public class EmployeeBL
+    {
+        public EmployeeDAL employeeDal;
+        public List<Employee> GetAllEmployees()
+        {
+            // We create an instance of the EmployeeDAL class to be able to invoke the SelectAllEmployees() method
+            // This is tight coupling because everytime the EmployeeDAL class changes, this class will also need to change
+            employeeDal = new EmployeeDAL();
+            return employeeDal.SelectAllEmployees();
+        }
+    }
+}
+
+
---
+++ dependency-injection-exercise/EmployeeDAL.cs
@@ -0,0 +1,49 @@
+﻿using dependency_injection_exercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dependency_injection_exercise
+{
+    //This class below demonstrate how EmployeeBL is tightly coupled with EmployeeDAL
+
+    public class EmployeeDAL
+    {
+        public List<Employee> SelectAllEmployees()
+        {
+            List<Employee> ListEmployees = new List<Employee>();
+            //Get Employees from Database (Hardcoded)
+
+            ListEmployees.Add(new Employee() { Id = 1, Name = "Harold", Department = "IT" });
+            ListEmployees.Add(new Employee() { Id = 2, Name = "Peter", Department = "HR" });
+            ListEmployees.Add(new Employee() { Id = 3, Name = "Annika", Department = "Payroll" });
+            return ListEmployees;
+        }
+    }
+    
+
+     //Using the Constructor Dependency Injection Design Pattern to make EmployeeBL and EmployeeDAL loosely coupled
+
+
+     // The dependency object should be interface-based, EmployeeDAL is the dependency object as this object is going
+     // to be used by the EmployeeBL class.
+    public interface IEmployeeDAL
+    {
+        List<Employee> SelectAllEmployees();
+    }
+
+    public class EmployeeDAL : IEmployeeDAL
+    {
+        public List<Employee> SelectAllEmployees()
+        {
+            List<Employee> ListEmployees = new List<Employee>();
+
+            ListEmployees.Add(new Employee() { Id = 1, Name = "Harold", Department = "IT" });
+            ListEmployees.Add(new Employee() { Id = 2, Name = "Peter", Department = "HR" });
+            ListEmployees.Add(new Employee() { Id = 3, Name = "Annika", Department = "Payroll" });
+            return ListEmployees;
+        }
+    }
+}
