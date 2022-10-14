9e4cf921895ab41947b1e84c0fd183c781b534ca
Daniela Escobar <daniela.b.escobar@gmail.com>
2022-10-14 09:25:27 +0200

    Remove dependency injection example to another branch

--- dependency-injection-exercise/EmployeeDAL.cs
+++ dependency-injection-exercise/EmployeeDAL.cs
@@ -22,28 +22,4 @@ namespace dependency_injection_exercise
             return ListEmployees;
         }
     }
-    
-
-     //Using the Constructor Dependency Injection Design Pattern to make EmployeeBL and EmployeeDAL loosely coupled
-
-
-     // The dependency object should be interface-based, EmployeeDAL is the dependency object as this object is going
-     // to be used by the EmployeeBL class.
-    public interface IEmployeeDAL
-    {
-        List<Employee> SelectAllEmployees();
-    }
-
-    public class EmployeeDAL : IEmployeeDAL
-    {
-        public List<Employee> SelectAllEmployees()
-        {
-            List<Employee> ListEmployees = new List<Employee>();
-
-            ListEmployees.Add(new Employee() { Id = 1, Name = "Harold", Department = "IT" });
-            ListEmployees.Add(new Employee() { Id = 2, Name = "Peter", Department = "HR" });
-            ListEmployees.Add(new Employee() { Id = 3, Name = "Annika", Department = "Payroll" });
-            return ListEmployees;
-        }
-    }
 }
