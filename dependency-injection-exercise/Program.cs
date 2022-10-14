3ee6f4062d5f0840abb01d3fdd71036f740a04a4
Daniela Escobar <daniela.b.escobar@gmail.com>
2022-10-14 09:30:35 +0200

    Add client code

--- dependency-injection-exercise/Program.cs
+++ dependency-injection-exercise/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using dependency_injection_exercise;
+using dependency_injection_exercise.Models;
+
+//Client code
+
+EmployeeBL employeeBL = new EmployeeBL(new EmployeeDAL());
+
+List<Employee> ListEmployee = employeeBL.GetAllEmployees();
+foreach(Employee employee in ListEmployee)
+{
+    Console.WriteLine($"Id = {employee.Id}, Name = {employee.Name}, Department = {employee.Department}");
+}
+Console.ReadKey();
